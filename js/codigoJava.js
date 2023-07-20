e1f2ff8450a4fbd5b5c8bb2f139dda0b11d91d4f
ElLucabum <135464054+ElLucabum@users.noreply.github.com>
2023-07-20 12:09:31 -0300

    Add files via upload

---
+++ js/codigoJava.js
@@ -0,0 +1,10 @@
+0 
+  function ModoOscuro() {document.body.style.backgroundImage = "url(./imagenes/osc-fondo-principal.jpg)";
+                         document.body.style.color = "white";
+                         
+  }
+  
+  function Copyright() {alert("Autor: Luca Benitez quero. Email: lucatizibentz05@gmail.com")
+	  
+	  
+  }

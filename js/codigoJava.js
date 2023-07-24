81da9dbff7621e4e7555c30938c95300bd356ebf
ElLucabum <135464054+ElLucabum@users.noreply.github.com>
2023-07-24 16:52:14 -0300

    Add files via upload

--- js/codigoJava.js
+++ js/codigoJava.js
@@ -1,10 +1,14 @@
 0 
   function ModoOscuro() {document.body.style.backgroundImage = "url(./imagenes/osc-fondo-principal.jpg)";
                          document.body.style.color = "white";
-                         
-  }
+                         document.getElementById("Colortexto").style.color = "#ff1850";
+                         document.getElementById("Colortexto2").style.color = "#ff1850";
+					     document.getElementById("Colortexto3").style.color = "#ff1850";
+					     document.getElementById("Colortexto4").style.color = "#ff1850";
+						 document.getElementById("Colortexto5").style.color = "#ff1850";
+    }                             
   
-  function Copyright() {alert("Autor: Luca Benitez quero. Email: lucatizibentz05@gmail.com")
+  function Copyright() {alert("Autor: Luca Benitez quero. Email: lucatizibentz05@gmail.com");
 	  
 	  
   }
